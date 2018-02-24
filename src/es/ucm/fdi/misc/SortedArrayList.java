7f34dcb38a26307dddb2161570293a465e7070a2
Talkal13 <pavazq01@ucm.es>
2018-02-24 14:28:55 +0100

    Added skeleton of SortedArrayList and Main

---
+++ src/es/ucm/fdi/misc/SortedArrayList.java
@@ -0,0 +1,39 @@
+package es.ucm.fdi.misc;
+
+import java.util.Collection;
+import java.util.Comparator;
+
+public class SortedArrayList<E> {
+	
+	public SortedArrayList(Comparator<E> comparator) {
+		
+	}
+	
+	public SortedArrayList() {
+		
+	}
+	
+	public boolean add(E element) {
+		return false;
+		
+	}
+	
+	public boolean addAll(Collection<? extends E> elements) {
+		return false;
+		
+	}
+	
+	public void add(int index, E element) {
+		
+	}
+	
+	public boolean addAll(int index, Collection<? extends E> elements) {
+		return false;
+		
+	}
+	
+	public void set(int index, E element) {
+		
+	}
+
+}
