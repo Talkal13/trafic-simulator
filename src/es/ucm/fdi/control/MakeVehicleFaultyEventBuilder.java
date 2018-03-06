9ecfeb48b6cfa33f747df6df01fc6edd3e0da7b0
usuario_local <usuario_local@PTO1113>
2018-03-06 11:22:18 +0100

    MakeFaulty

---
+++ src/es/ucm/fdi/control/MakeVehicleFaultyEventBuilder.java
@@ -0,0 +1,25 @@
+package es.ucm.fdi.control;
+
+import es.ucm.fdi.ini.IniSection;
+import es.ucm.fdi.model.Event;
+import es.ucm.fdi.model.MakeVehicleFaultyEvent;
+
+public class MakeVehicleFaultyEventBuilder extends EventBuilder {
+	
+	public static final String ID = "make_vehicle_faulty";
+	public static final String IDToString = "Make Vehicle Faulty";
+	
+	
+	public Event parse(IniSection section) {
+		if(!section.getTag().equals(ID))
+			return null;
+		else
+			return new MakeVehicleFaultyEvent(EventBuilder.parseNonNegInt(section, "time", 0), EventBuilder.parseListValidId(section, "vehicles"), EventBuilder.parsePositiveInt(section, "duration"));
+			
+	}
+	
+	public String toString(){
+		return IDToString;
+		
+	}
+}
